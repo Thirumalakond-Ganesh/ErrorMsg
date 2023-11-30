45ba4334895580beb40ec8ce4db065d7359029dd
Thirumalakonda-Ganesh <ganitthirumala50@gmail.com>
2023-11-30 22:30:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from "react";
+import { useForm, FormProvider } from "react-hook-form";
 import './App.css';
 
-function App() {
+const MyForm = () => {
+  const methods = useForm();
+  const { handleSubmit, register, setError, formState: { errors } } = methods;
+
+  const onSubmit = (data) => {
+    // Perform form submission logic
+
+    // Example: Manually set an error for a specific field
+    setError("inputA", {
+      type: "manual",
+      message: "This is a custom error message for Input A",
+    });
+
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="inputA">Input A</label>
+      <input {...register("inputA", { required: true })} />
+      {errors.inputA && <p>{errors.inputA.message}</p>} {/* Display error message */}
+
+      <label htmlFor="inputB">Input B</label>
+      <input {...register("inputB", { required: true })} />
+      {errors.inputB && <p>{errors.inputB.message}</p>} {/* Display error message */}
+
+      <button type="submit">Submit</button>
+    </form>
   );
-}
+};
+
+const App = () => {
+  const methods = useForm();
+
+  return (
+    <FormProvider {...methods}>
+      <MyForm />
+    </FormProvider>
+  );
+};
 
 export default App;
